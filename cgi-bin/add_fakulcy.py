bc0b30f41d63311d91adf5ce7e47d8b087f4714b
MaximIsupov <59082609+MaximIsupov@users.noreply.github.com>
2021-04-12 12:14:40 +0300

    Update add_fakulcy.py

--- cgi-bin/add_fakulcy.py
+++ cgi-bin/add_fakulcy.py
@@ -10,6 +10,40 @@ cursor = conn.cursor()
 
 cursor.execute("insert into fakulcies values(?)", (name, ))
 
+cursor.execute("select * from students")
+studs = cursor.fetchall()
+
+cursor.execute("select * from groups")
+grou = cursor.fetchall()
+
+cursor.execute("select * from fakulcies")
+faks = cursor.fetchall()
+
+print("Content-type: text/html")
+print()
+print("<h1> Студенты: </h1>")
+for line in studs:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
+print()
+print("<h1> Группы: </h1>")
+for line in grou:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
+print("<h1> Факультеты: </h1>")
+for line in faks:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
+
 conn.commit()
 cursor.close()
 conn.close()
