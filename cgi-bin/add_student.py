c2ad9bd34c3b64dbb774f6a297a3bbcb7a0de94f
MaximIsupov <59082609+MaximIsupov@users.noreply.github.com>
2021-04-12 12:14:57 +0300

    Update add_student.py

--- cgi-bin/add_student.py
+++ cgi-bin/add_student.py
@@ -17,8 +17,42 @@ cursor = conn.cursor()
 
 cursor.execute("insert into students values(?, ?, ?, ?)", students)
 
+cursor.execute("select * from students")
+studs = cursor.fetchall()
+
+cursor.execute("select * from groups")
+grou = cursor.fetchall()
+
+cursor.execute("select * from fakulcies")
+faks = cursor.fetchall()
+
+print("Content-type: text/html")
+print()
+print("<h1> Студенты: </h1>")
+for line in studs:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
+print()
+print("<h1> Группы: </h1>")
+for line in grou:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
+print("<h1> Факультеты: </h1>")
+for line in faks:
+    print("<div>")
+    for el in line:
+        print(el, " ", end='')
+    print("</div>")
+
 conn.commit()
 cursor.close()
 conn.close()
+
 
 
